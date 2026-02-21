d7efc83e49db6c053be261daf396015be7f42576
Radu Edward-Andrei <raduedwardandrei@gmail.com>
2026-02-21 16:42:55 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Radu Edward-Andrei on 21.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
